f8349599f1eb094032f01c9fe9eb10250b3f5948
Paul Tuck <paul.tuck@oxon.org>
2023-11-23 18:26:34 +0700

    "2023-11-23_18:26_UTC+7"

---
+++ docs/social_auth.md
@@ -0,0 +1,28 @@
+## Google
+
+id: A unique identifier for the user.
+email: The user's email address.
+name: The user's full name.
+given_name: The user's given (first) name.
+family_name: The user's family (last) name.
+picture: URL of the user's profile picture.
+locale: The user's locale (e.g., en).
+Facebook:
+
+id: A unique identifier for the user.
+name: The user's full name.
+first_name: The user's first name.
+last_name: The user's last name.
+email: The user's email address.
+picture: The user's profile picture.
+gender: The user's gender.
+locale: The user's locale.
+Twitter:
+
+id_str: A string representing the user's unique ID.
+name: The user's name as they've defined it.
+screen_name: The user's Twitter handle.
+location: The user-reported location.
+description: The user's profile description.
+url: URL provided by the user in their profile.
+profile_image_url: URL of the user's profile image.
--- src/modules/users/types/user.type.ts
+++ src/modules/users/types/user.type.ts
@@ -1,35 +1,59 @@
-export interface Address {
-	city: string;
-	state: string;
-	country: string;
-	postalCode: string;
+export interface BaseUser {
+	userId: string; // Assigned by Mongoose, not optional
 }
 
-export interface User {
-	userId: string;
+export interface UserAuthentication extends BaseUser {
 	facebookId?: string;
 	googleId?: string;
 	twitterId?: string;
-	username: string;
-	email: string;
-	password: string;
-	givenName: string;
-	familyName: string;
-	preferredName: string;
-	profilePicture: string; // URL or path to the image
-	birthDate: Date;
-	gender: string;
-	phoneNumber: string;
-	address: Address;
-	isVerified: boolean;
-	twoFactorEnabled: boolean;
-	language: string;
-	locale: string;
-	timeZone: string;
-	roles: string[];
-	isActive: boolean;
-	isSuspended: boolean;
-	createdAt: Date;
-	updatedAt: Date;
-	lastLogin: Date;
+	email?: string;
+	password?: string;
 }
+
+export interface UserProfile extends BaseUser {
+	username?: string;
+	givenName?: string;
+	familyName?: string;
+	preferredName?: string;
+	birthDate?: Date;
+	gender?: string;
+	phoneNumber?: string;
+}
+
+export interface UserPicture extends BaseUser {
+	picture?: string;
+}
+
+export interface UserAddress extends BaseUser {
+	streetName?: string;
+	streetNumber?: string;
+	city?: string;
+	state?: string;
+	country?: string;
+	postalCode?: string;
+}
+
+export interface UserAccountStatus extends BaseUser {
+	isVerified?: boolean;
+	twoFactorEnabled?: boolean;
+	language?: string;
+	locale?: string;
+	timeZone?: string;
+	roles?: string[];
+	isActive?: boolean;
+	isSuspended?: boolean;
+}
+
+export interface UserAuditInfo extends BaseUser {
+	createdAt?: Date;
+	updatedAt?: Date;
+	lastLogin?: Date;
+}
+
+export interface User
+	extends UserAuthentication,
+		UserProfile,
+		UserPicture,
+		UserAddress,
+		UserAccountStatus,
+		UserAuditInfo {}
